c7ca5c223c2a0a75d3555ad03b3cdd0448f5f3dc
Asif Syed <Asif.Syed@cytel.pune.in.local>
2020-06-08 16:26:14 +0530

    Ignored some files from code coverage

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import store from './redux';
 
-
+/* istanbul ignore next */
 ReactDOM.render(
-  <Provider store = {store}>
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<Router>
+				<App />
+			</Router>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root')
 );
-
-
-
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,12 @@
+/* istanbul ignore file */
+
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(
-    rootReducer,
-    applyMiddleware(sagaMiddleware)
-    )
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 export default store;
